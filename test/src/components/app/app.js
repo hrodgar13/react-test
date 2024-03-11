e14c1803c72c401626bd85640b6f23cdea9f4a93
Illia Yermackov <ermackov2003@gmail.com>
2024-03-11 12:00:12 +0200

    form working

--- test/src/components/app/app.js
+++ test/src/components/app/app.js
@@ -1,43 +1,80 @@
-import React from "react";
+import React, {Component} from "react";
 import './app.css'
-import {Input} from "../input/input";
 import {Title} from "../title/title";
+import Input from "../input/input";
 
-export const App = () => {
-    return (
-        <form className={'main-form'}>
-            <div className={'main-form-part full'}>
-                <div className={'structure-block full'}>
-                    <Title title={'Full name'}/>
-                    <div className={'input-block'}>
-                        <Input label={'First Name'} id={'firstname'}/>
-                        <Input label={'Last Name'} id={'lastname'}/>
+export default class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            formObj: {
+                firstname: null,
+                lastname: null,
+                streetAddr1: null,
+                streetAddr2: null,
+                city: null,
+                state: null,
+                postal: null,
+                phone: null,
+                email: null
+            }
+        }
+    }
+
+
+
+    setValueToObj = ({field, value}) => {
+        this.setState(({formObj}) => ({
+            formObj: {
+                ...formObj,
+                [field]: value
+            }
+        }));
+    }
+
+    sendForm = (e) => {
+        e.preventDefault()
+        console.log(this.state.formObj);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.sendForm}  className={'main-form'}>
+                <div className={'main-form-part full'}>
+                    <div className={'structure-block full'}>
+                        <Title title={'Full name'}/>
+                        <div className={'input-block'}>
+                            <Input setValueToObj={this.setValueToObj} label={'First Name'} id={'firstname'}/>
+                            <Input setValueToObj={this.setValueToObj} label={'Last Name'} id={'lastname'}/>
+                        </div>
+                    </div>
+                    <div className={'structure-block full'}>
+                        <Title title={'Address'}/>
+                        <Input setValueToObj={this.setValueToObj} id={'streetAddr1'} label={'Street Address'}/>
+                        <Input setValueToObj={this.setValueToObj} id={'streetAddr2'} label={'Street Address Line 2'}/>
+                        <div className={'input-block'}>
+                            <Input setValueToObj={this.setValueToObj} id={'city'} label={'City'}/>
+                            <Input setValueToObj={this.setValueToObj} id={'state'} label={'State / Province'}/>
+                        </div>
+                        <Input setValueToObj={this.setValueToObj} id={'postal'} label={'Postal / Zip Code'}/>
+                    </div>
+                    <div className={'structure-block half'}>
+                        <Title title={'Phone number'}/>
+                        <Input setValueToObj={this.setValueToObj} id={'phone'}/>
+                    </div>
+
+                    <div className={'structure-block half'}>
+                        <Title setValueToObj={this.setValueToObj} title={'E-mail'}/>
+                        <Input setValueToObj={this.setValueToObj} id={'email'} type={'email'}/>
                     </div>
                 </div>
-                <div className={'structure-block full'}>
-                    <Title title={'Address'}/>
-                    <Input id={'streetAddr1'} label={'Street Address'}/>
-                    <Input id={'streetAddr2'} label={'Street Address Line 2'}/>
-                    <div className={'input-block'}>
-                        <Input id={'city'} label={'City'}/>
-                        <Input id={'state'} label={'State / Province'}/>
-                    </div>
-                    <Input className={'postal'} label={'Postal / Zip Code'}/>
+                <div className={'horizontalLine'}/>
+                <div className={'main-form-part'}>
+                    <button type={'submit'}> Submit</button>
                 </div>
-                <div className={'structure-block half'}>
-                    <Title title={'Phone number'}/>
-                    <Input id={'phone'}/>
-                </div>
-
-                <div className={'structure-block half'}>
-                    <Title title={'E-mail'}/>
-                    <Input id={'email'} type={'email'}/>
-                </div>
-            </div>
-            <div className={'horizontalLine'}/>
-            <div className={'main-form-part'}>
-                <button type={'submit'}> Submit </button>
-            </div>
-        </form>
-    )
+            </form>
+        )
+    }
 }
--- test/src/components/input/input.js
+++ test/src/components/input/input.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import './input.css'
-export const Input = ({label, id, type = 'text'}) => {
-    return (
-        <div className={'input-container'}>
-            <input id={id} type={type} />
-            <label htmlFor={id}> {label} </label>
-        </div>
-    )
+export default class Input extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            value: ''
+        }
+    }
+
+    onSetValueToObj = (e) => {
+        const value = e.target.value
+        const id = this.props.id
+
+        this.setState({value})
+        this.props.setValueToObj({field: id, value})
+    }
+
+    render () {
+        const {label, id, type = 'text'} = this.props
+
+        return (
+            <div className={'input-container'}>
+                <input
+                    id={id} type={type}
+                    value={this.state.value}
+                    onChange={this.onSetValueToObj}
+                />
+                <label htmlFor={id}> {label} </label>
+            </div>
+        )
+    }
 }
